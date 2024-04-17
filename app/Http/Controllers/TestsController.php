81ba1f0670b34053987a47b22ebef77001ed7962
aricha_samson <samaricha001@gmail.com>
2024-04-17 14:10:10 +0300

    adding test controller method and routes

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -6,5 +6,61 @@ use Illuminate\Http\Request;
 
 class TestsController extends Controller
 {
-    //
+    public function index()
+    {
+      $posts = Post::all();
+      return view('tasks.index', compact('tasks'));
+    }
+
+    public function store(Request $request)
+    {
+      $request->validate([
+        'task_name' => 'required|max:255',
+        'priority' => 'required|max:255',
+        'status' => 'required|max:255',
+        'date' => 'required|max:255',
+      ]);
+      Task::create($request->all());
+      return redirect()->route('tasks.index')
+        ->with('success', 'Task created successfully.');
+    }
+   
+    public function update(Request $request, $id)
+    {
+      $request->validate([
+        'task_name' => 'required|max:255',
+        'priority' => 'required|max:255',
+        'status' => 'required|max:255',
+        'date' => 'required|max:255',
+      ]);
+      $post = Task::find($id);
+      $post->update($request->all());
+      return redirect()->route('tasks.index')
+        ->with('success', 'Post updated successfully.');
+    }
+
+    public function destroy($id)
+    {
+      $post = Task::find($id);
+      $post->delete();
+      return redirect()->route('tasks.index')
+        ->with('success', 'Post deleted successfully');
+    }
+
+    public function create()
+    {
+      return view('tasks.create');
+    }
+
+    public function show($id)
+    {
+      $post = Task::find($id);
+      return view('tasks.show', compact('post'));
+    }
+
+    public function edit($id)
+    {
+      $post = Task::find($id);
+      return view('tasks.edit', compact('post'));
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// returns  a page with all tests
+Route::get('/',[TestsController::class .'index'])->name('tests.index');
+// returns the form for adding a test
+Route::get('/tests/create',[TestsController::class, 'create'])->name('tests.create');
+// adds a task to the database
+Route::post('/tests', [TaskController::class, 'store'])->name('tests.store');
+// returns a page that shows a full test
+Route::get('/tests/{task}', [TestsController::class, 'show'])->name('tests.show');
+// returns the form for editing a test
+Route::get('/tests/{task}/edit', [TestsController::class, 'edit'])->name('tests.edit');
+// updates a test
+Route::put('/tests/{task}', [TestsController::class, 'update'])->name('tests.update');
+// deletes a test
+Route::delete('/tests/{task}', [TestsController::class, 'destroy'])->name('tests.destroy');
